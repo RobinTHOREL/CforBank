b2e07cd9a03b08b7b841318f895415d4e6ae8aae
brixton <thorelrobin@yahoo.fr>
2017-07-04 18:51:17 +0200

    First commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("--- C for Bank\n");
+    printf("--------\n");
+    printf("--- Menu\n");
+    printf("--------\n");
+    printf("Faites un choix : \n");
+    printf("1. Gestion clientele,\n");
+    printf("2. Gestion des comptes,\n");
+    printf("3. Gestion des operation\n");
+    return 0;
+}
