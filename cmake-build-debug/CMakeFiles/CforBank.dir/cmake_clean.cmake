73dd9d1b15e156af0328e2600bd8388554440551
younesdiouri <younesdiouri@gmail.com>
2017-07-04 20:20:41 +0200

    push du main oklm

---
+++ cmake-build-debug/CMakeFiles/CforBank.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CforBank.dir/main.c.obj"
+  "CforBank.pdb"
+  "CforBank.exe"
+  "CforBank.exe.manifest"
+  "libCforBank.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CforBank.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.c
+++ main.c
@@ -12,13 +12,28 @@ typedef struct client Client;
 void ajouter_client(Client *c);
 void  modifier_client(Client rep[], int nb);
 int main() {
-    printf("--- C for Bank\n");
-    printf("--------\n");
-    printf("--- Menu\n");
-    printf("--------\n");
-    printf("Faites un choix : \n");
-    printf("1. Gestion clientele,\n");
-    printf("2. Gestion des comptes,\n");
-    printf("3. Gestion des operation\n");
-    return 0;
+    char choix;
+      do{
+            printf("--- C for Bank\n");
+            printf("--------\n");
+            printf("--- Menu\n");
+            printf("--------\n");
+            printf("Faites un choix : \n");
+            printf("1. Gestion clientele,\n");
+            printf("2. Gestion des comptes,\n");
+            printf("3. Gestion des operation\n");
+            printf("Q. Quitter\n");
+
+            scanf("%c", &choix);
+          printf("%c",choix);
+
+        }while(choix != '1' && choix != '2' && choix != '3');
+    switch (choix)
+        case '1' :
+
+
+
+
+
+            return 0;
 }
